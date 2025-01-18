79c6fbe4019685fec862d86e3d912d7106a365f1
fpaulovich <f.paulovich@tue.nl>
2025-01-18 11:47:33 +0100

    Restructuring the code

--- sequence_view_main.py
+++ sequence_view_main.py
@@ -14,7 +14,15 @@ def diff():
     return diff_str
 
 
+def concat_e_pi():
+    concat = ''
+    for i in range(min(len(pi_number), len(e_number))):
+        concat = concat + pi_number[i] + e_number[i]
+    return concat
+
+
 if __name__ == '__main__':
     sequence_view(e_number, 'e.svg', nr_rows=30, nr_columns=30, radius=5)
     sequence_view(pi_number, 'pi.svg', nr_rows=30, nr_columns=30, radius=5)
     sequence_view(diff(), 'diff.svg', nr_rows=30, nr_columns=30, radius=5)
+    sequence_view(concat_e_pi(), 'concat_e_pi.svg', nr_rows=30, nr_columns=30, radius=5)
